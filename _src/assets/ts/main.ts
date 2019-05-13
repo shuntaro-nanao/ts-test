d0a79bc506146759e438cc0a207a8d8077437c22
shuntaro-nanao <nanao@webdrops.jp>
2019-05-13 16:59:01 +0900

    tsでjquery使えるようにしてみた

--- _src/assets/ts/main.ts
+++ _src/assets/ts/main.ts
@@ -1,2 +1,16 @@
-const message:string = 'hoge';
-console.log(message);
+$(function(){
+
+  const message:string = 'hoge';
+  if(message){
+    console.log(message);
+  }
+
+  const arr = [ 10 , 20 , 30 ];
+  for(let i in arr){
+    console.log(arr[i]);
+  }
+
+
+  $('#change').addClass('hoge');
+
+});
