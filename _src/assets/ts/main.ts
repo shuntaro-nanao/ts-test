0b3c0f221683d638444929b3fe51972ece01d7fb
shuntaro-nanao <nanao@webdrops.jp>
2019-05-30 18:53:27 +0900

    関数を書いてみた

--- _src/assets/ts/main.ts
+++ _src/assets/ts/main.ts
@@ -12,6 +12,29 @@ $(function(){
     console.log(arr[i]);
   }
 
+  //引数に型を設定、()の隣は返り値の型
+  function omikuji(age : number , name : string) : string {
+    let unn : string = '';
+    if(age >= 17){
+      unn = '大吉';
+    }else{
+      unn = '凶';
+    }
+    return name + 'さんの運勢は' + unn + 'です。';
+  }
+  console.log(omikuji(0 , '木村'));
+
+  //省略可能な引数には ? をつける
+  function ageCount(age? : number) : number {
+    let ageeeee : number = 0;
+    if(!age){
+      ageeeee = 1;
+    }
+    return ageeeee;
+  }
+  //空が通る
+  console.log(ageCount());
+
 
   $('#change').addClass('hoge');
 
