04752f866db540573f3fdd5d009898fd816b6037
shuntaro0319 <shuntaro.nno0319@gmail.com>
2020-09-23 20:31:46 +0900

    ts実行環境構築

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
-const webpack = require('webpack');
-const path = require('path');
-
 module.exports = {
-  mode: 'production',
-  entry: './_src/assets/ts/main.ts', //このファイルにinportを記述する
-  output: {
-    path: path.join(__dirname, '_src/assets/js'), //出力先ディレクトリ
-    filename: 'script.js', //出力ファイル名
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/, // 拡張子 .ts の場合
-        use: 'ts-loader' // TypeScript をコンパイルする
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts'] // import 文で .ts ファイルを解決するため
-  }
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+    entry: `${__dirname}/_src/ts/index.ts`,
+    // ファイルの出力設定
+    output: {
+        // 出力ファイルのディレクトリ名
+        path: `${__dirname}/assets/js/`,
+        // 出力ファイル名
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+            },
+        ],
+    },
+    // import 文で .ts ファイルを解決するため
+    // これを定義しないと import 文で拡張子を書く必要が生まれる。
+    // フロントエンドの開発では拡張子を省略することが多いので、
+    // 記載したほうがトラブルに巻き込まれにくい。
+    resolve: {
+      // 拡張子を配列で指定
+        extensions: [
+            '.ts', '.js',
+        ],
+    },
+    devServer: { 
+        contentBase: `${__dirname}/`,
+    }
 };
